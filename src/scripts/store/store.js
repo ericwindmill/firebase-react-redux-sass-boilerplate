0a7f096df094a415e64df53d267518f287603300
Eric Windmill <ericwindmill@gmail.com>
2017-06-29 14:35:54 -0700

    Update store w. preloaded state

--- src/scripts/store/store.js
+++ src/scripts/store/store.js
@@ -2,8 +2,10 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import RootReducer from '../reducers/root_reducer'
 
-const configureStore = () => {
-  return createStore(RootReducer, applyMiddleware(thunk))
-};
+const configureStore = (preLoadedState = {}) => (
+  createStore(RootReducer,
+  preLoadedState,
+  applyMiddleware(thunk))
+)
 
 export default configureStore
