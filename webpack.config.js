ba82d0c6119a531e0d24146a99e94cc60dbdcd97
Eric Windmill <ericwindmill@gmail.com>
2017-07-02 18:23:17 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     loaders: [
       {
         test: [/\.jsx?$/, /\.js?$/],
-        exclude: '/node_modules/',
+        exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']
@@ -20,7 +20,7 @@ module.exports = {
       {
         test: /\.scss$/,
         loaders: ["style", "css", "sass"],
-        exclude: "/node_modules"
+        exclude: /(node_modules)/
       }
     ]
   },
